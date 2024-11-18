b6813fa23909950bb3f50fd66dc8343034655716
Aalok <alokdev80@gmail.com>
2024-11-18 15:09:16 +0530

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './css/home.css'
 import Home from './componets/Home';
 
 function App() {
+  console.log("API Key:", process.env.REACT_APP_API_KEY);
+  console.log("API Key:", process.env.REACT_APP_BASE_URL);
+  console.log("API Key:", process.env.REACT_APP_DAY);
   return (
     <div className="app">
       <Home />
